872deda105065bbf0854efce181b3df6fa5aab8a
Mykola Koval <mykola_koval@epam.com>
2013-11-04 14:23:25 +0200

    test image added

--- VideoRecorder/vrecorder-ui/src/main/java/com/atanor/vrecorder/client/ui/MainPane.java
+++ VideoRecorder/vrecorder-ui/src/main/java/com/atanor/vrecorder/client/ui/MainPane.java
@@ -1,6 +1,8 @@
 package com.atanor.vrecorder.client.ui;
 
+import com.smartgwt.client.widgets.Canvas;
 import com.smartgwt.client.widgets.IButton;
+import com.smartgwt.client.widgets.Img;
 import com.smartgwt.client.widgets.Label;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
@@ -15,13 +17,27 @@ public class MainPane extends HLayout {
 	private MainPanePresenter presenter;
 	private final IButton startRecord;
 	private final IButton stopRecord;
+	private final Canvas imageBox;
 	
 	public MainPane() {
 		setWidth100();
 		setHeight100();
+		setBackgroundColor("lightgrey");
 
+		imageBox = new Canvas();
+		imageBox.setWidth(265);
+		imageBox.setHeight(200);
+		imageBox.setShowEdges(true);
+		imageBox.setBackgroundColor("white");
+
+//		final Img img = new Img();
+//		img.setWidth(60);
+//		img.setHeight(60);		
+//		img.setSrc("image3.png");
+//		imageBox.addChild(img);
+		
 		startRecord = new IButton("Start Recording");
-		startRecord.setWidth(90);	
+		startRecord.setWidth(90);
 		startRecord.addClickHandler(new ClickHandler() {
 
 			@Override
@@ -41,8 +57,11 @@ public class MainPane extends HLayout {
 			}
 		});
 
+		final HLayout spacer = new HLayout();
+		spacer.setHeight(20);
+
 		final HLayout headerPane = new HLayout();
-		headerPane.addMembers(startRecord, stopRecord);
+		headerPane.addMembers(imageBox, startRecord, stopRecord);
 		headerPane.setMembersMargin(10);
 
 		final ListGrid listGrid = new ListGrid();
@@ -56,24 +75,29 @@ public class MainPane extends HLayout {
 		final VLayout vLayout = new VLayout();
 		vLayout.setWidth("80%");
 		vLayout.setHeight100();
-		vLayout.setMembersMargin(50);
-		vLayout.addMembers(headerPane, listGrid, freeSpace);
+		vLayout.setMembersMargin(20);
+		vLayout.addMembers(spacer, headerPane, listGrid, freeSpace);
 
 		addMembers(new LayoutSpacer(), vLayout, new LayoutSpacer());
 	}
 
-	public void onRecordingStarted(){
+	public void onRecordingStarted() {
 		startRecord.disable();
 		stopRecord.enable();
+		
+		final Img img = new Img();
+		img.setWidth(60);
+		img.setHeight(60);		
+		img.setSrc("image1.png");
+		imageBox.addChild(img);
 	}
-	
-	public void onRecordingStopped(){
+
+	public void onRecordingStopped() {
 		startRecord.enable();
 		stopRecord.disable();
 	}
-	
+
 	public void setPresenter(final MainPanePresenter presenter) {
 		this.presenter = presenter;
 	}
 }
-
