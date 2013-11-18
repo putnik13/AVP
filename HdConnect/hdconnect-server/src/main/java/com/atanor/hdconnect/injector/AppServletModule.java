0121a777a49616364a7c8d9fde2d19fd619d75bc
MykolaKoval <mykola.koval@gmail.com>
2013-11-18 02:59:14 +0200

    cleanup

--- HdConnect/hdconnect-server/src/main/java/com/atanor/hdconnect/injector/AppServletModule.java
+++ HdConnect/hdconnect-server/src/main/java/com/atanor/hdconnect/injector/AppServletModule.java
@@ -18,17 +18,9 @@ public class AppServletModule extends ServletModule {
 		bind(ManagedGWTResource.class);
 		bind(AtmosphereServlet.class).in(Scopes.SINGLETON);
 		
-//		bind(new TypeLiteral<Map<String, String>>() {}).
-//		annotatedWith(Names.named(AtmosphereGuiceServlet.JERSEY_PROPERTIES)).toInstance(Collections.<String, String> emptyMap());
-//		
 		final Map<String, String> params = Maps.newHashMap();
-		//params.put(PackagesResourceConfig.PROPERTY_PACKAGES, "com.atanor.hdconnect.server");
 		params.put("org.atmosphere.cpr.packages", "com.atanor.hdconnect.server");
-//		params.put("org.atmosphere.useWebSocket", "true");
-//		params.put("org.atmosphere.useNative", "true");
-
 		serve(BASE_URL + "/atmosphere/*").with(AtmosphereServlet.class, params);
-		//serve(BASE_URL + "/rest/*").with(GuiceContainer.class);
 	}
 
 }
