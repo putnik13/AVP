f79e2154fea7cd719280bd8de78850ea0e02e6a1
MykolaKoval <mykola.koval@gmail.com>
2013-11-28 21:07:41 +0200

    added navigation controls

--- VideoServer/vs-admin-ui/src/main/java/com/atanor/vserver/vsadmin/client/ui/NavigatePane.java
+++ VideoServer/vs-admin-ui/src/main/java/com/atanor/vserver/vsadmin/client/ui/NavigatePane.java
@@ -7,50 +7,52 @@ import com.atanor.vserver.vsadmin.client.events.SectionAnimationStartedHandler;
 import com.atanor.vserver.vsadmin.client.events.SectionSelectedEvent;
 import com.atanor.vserver.vsadmin.client.ui.sections.Section;
 import com.google.web.bindery.event.shared.EventBus;
-import com.smartgwt.client.widgets.IButton;
+import com.smartgwt.client.types.Alignment;
+import com.smartgwt.client.widgets.Canvas;
+import com.smartgwt.client.widgets.Img;
 import com.smartgwt.client.widgets.events.ClickEvent;
 import com.smartgwt.client.widgets.events.ClickHandler;
+import com.smartgwt.client.widgets.events.MouseOutEvent;
+import com.smartgwt.client.widgets.events.MouseOutHandler;
+import com.smartgwt.client.widgets.events.MouseOverEvent;
+import com.smartgwt.client.widgets.events.MouseOverHandler;
 import com.smartgwt.client.widgets.layout.HLayout;
+import com.smartgwt.client.widgets.layout.LayoutSpacer;
 import com.smartgwt.client.widgets.layout.VLayout;
 
 public class NavigatePane extends HLayout implements SectionAnimationStartedHandler {
 
+	private static final Integer CONTROL_SIZE = 48;
+	private static final int CONTROL_SIZE_HOVER = 60;
+	
 	@Inject
 	private EventBus eventBus;
 
-	final IButton streamControl;
-	final IButton shareConference;
-	final IButton broadcasting;
-	final IButton editConfiguration;
+	final Canvas streamControl;
+	final Canvas shareConference;
+	final Canvas broadcasting;
+	final Canvas editConfiguration;
 
 	public NavigatePane() {
-		setWidth(200);
+		setWidth(150);
 		setHeight100();
-		//setBackgroundColor("yellow");
 		setBackgroundColor("grey");
-		
-		streamControl = new IButton("Stream Control");
-		streamControl.setWidth100();
-		streamControl.addClickHandler(buildClickHandler(Section.STREAM_CONTROL));
+
+		streamControl = createNavigateControl(Section.STREAM_CONTROL, "stream_control.png", "Stream Control");
 		streamControl.setDisabled(true);
-		
-		shareConference = new IButton("Share Conference");
-		shareConference.setWidth100();
-		shareConference.addClickHandler(buildClickHandler(Section.SHARE_CONFERENCE));
-
-		broadcasting = new IButton("Broadcasting");
-		broadcasting.setWidth100();
-		broadcasting.addClickHandler(buildClickHandler(Section.BROADCASTING));
-
-		editConfiguration = new IButton("Edit Configuration");
-		editConfiguration.setWidth100();
-		editConfiguration.addClickHandler(buildClickHandler(Section.EDIT_CONFIGURATION));
+		shareConference = createNavigateControl(Section.SHARE_CONFERENCE, "share_conference.png", "Share Conference");
+		broadcasting = createNavigateControl(Section.BROADCASTING, "broadcasting.png", "Broadcasting");
+		editConfiguration = createNavigateControl(Section.EDIT_CONFIGURATION, "edit_config.png", "Edit Configuration");
 
 		final VLayout controlsLayout = new VLayout();
 		controlsLayout.setWidth100();
 		controlsLayout.setHeight100();
+		controlsLayout.setMembersMargin(10);
+		controlsLayout.setDefaultLayoutAlign(Alignment.CENTER);
 
-		controlsLayout.addMembers(streamControl, shareConference, broadcasting, editConfiguration);
+		controlsLayout.addMembers(new LayoutSpacer(), streamControl, shareConference, broadcasting, editConfiguration,
+				new LayoutSpacer());
+
 		addMember(controlsLayout);
 	}
 
@@ -65,10 +67,16 @@ public class NavigatePane extends HLayout implements SectionAnimationStartedHand
 	}
 
 	private void enableAllControls() {
-		streamControl.enable();
-		shareConference.enable();
-		broadcasting.enable();
-		editConfiguration.enable();
+		resetControl(streamControl);
+		resetControl(shareConference);
+		resetControl(broadcasting);
+		resetControl(editConfiguration);
+	}
+
+	private void resetControl(final Canvas control) {
+		control.enable();
+		control.setWidth(CONTROL_SIZE);
+		control.setHeight(CONTROL_SIZE);
 	}
 
 	@Override
@@ -90,5 +98,30 @@ public class NavigatePane extends HLayout implements SectionAnimationStartedHand
 		default:
 			throw new IllegalStateException("Section not found " + event.getSection().name());
 		}
+	}
+
+	private Canvas createNavigateControl(final Section section, final String imgSource, final String tooltip) {
+		final Img img = new Img();
+		img.setSrc(imgSource);
+		img.setTooltip(tooltip);
+		img.setWidth(CONTROL_SIZE);
+		img.setHeight(CONTROL_SIZE);
+		img.addClickHandler(buildClickHandler(section));
+		img.addMouseOverHandler(new MouseOverHandler() {
+
+			@Override
+			public void onMouseOver(MouseOverEvent event) {
+				img.animateResize(CONTROL_SIZE_HOVER, CONTROL_SIZE_HOVER, null, 100);
+			}
+		});
+		img.addMouseOutHandler(new MouseOutHandler() {
+
+			@Override
+			public void onMouseOut(MouseOutEvent event) {
+				img.animateResize(CONTROL_SIZE, CONTROL_SIZE);
+			}
+		});
+
+		return img;
 	}
 }
